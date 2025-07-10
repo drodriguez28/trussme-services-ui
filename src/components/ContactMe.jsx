817b60accbfd0fa4a1df2d382aca53fd8c0e1377
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2025-07-10 09:43:33 -0400

    added the api in a env file

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -28,7 +28,7 @@ function ContactMe() {
     setShowError(false);
 
     try {
-      const response = await fetch("http://localhost:3001/send-email", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/send-email`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
