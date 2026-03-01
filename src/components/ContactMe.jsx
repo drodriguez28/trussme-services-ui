405f05a0dd346e2dd30fba471a13ef537e1c5d84
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2026-03-01 12:35:38 -0500

    fixed bug

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Form,
   FormControl,
@@ -58,6 +58,10 @@ function ContactMe() {
       setSubmitted(false);
     }
   };
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
