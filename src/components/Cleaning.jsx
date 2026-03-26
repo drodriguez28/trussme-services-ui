50e53021c4011acb44e5167087bbfa7960bae133
Deivid Rodriguez <rodriguezdeivid17@gmail.com>
2026-03-26 00:15:52 -0400

    added pics

--- src/components/Cleaning.jsx
+++ src/components/Cleaning.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Table } from "@contentful/f36-components";
+import Carousel from "../reuse/Carousel";
 import { Link } from "react-router-dom";
 
 function Cleaning() {
@@ -32,24 +33,41 @@ function Cleaning() {
   return (
     <div className="max-w-3xl mx-auto mt-10 px-4">
       <h1 className="text-3xl font-bold mb-8 text-white">Cleaning</h1>
-      <Table>
-        <Table.Head>
-          <Table.Row>
-            <Table.Cell>Service</Table.Cell>
-            <Table.Cell>Type</Table.Cell>
-            <Table.Cell>Description</Table.Cell>
-          </Table.Row>
-        </Table.Head>
-        <Table.Body>
-          {contentTypes.map((service) => (
-            <Table.Row key={service.id}>
-              <Table.Cell>{service.name}</Table.Cell>
-              <Table.Cell>{service.type}</Table.Cell>
-              <Table.Cell>{service.description}</Table.Cell>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+        <div className="w-full max-w-md my-8">
+          <Carousel
+            images={[
+              "/images/clean1.png",
+              "/images/clean2.png",
+              "/images/clean3.png",
+              "/images/clean4.png",
+              "/images/clean5.png",
+              "/images/clean6.png",
+              "/images/clean7.png",
+              "/images/clean8.png",
+              "/images/clean9.png",
+            ]}
+          />
+        </div>
+        <Table>
+          <Table.Head>
+            <Table.Row>
+              <Table.Cell>Service</Table.Cell>
+              <Table.Cell>Type</Table.Cell>
+              <Table.Cell>Description</Table.Cell>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+          </Table.Head>
+          <Table.Body>
+            {contentTypes.map((service) => (
+              <Table.Row key={service.id}>
+                <Table.Cell>{service.name}</Table.Cell>
+                <Table.Cell>{service.type}</Table.Cell>
+                <Table.Cell>{service.description}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
       <div className="mt-8 ">
         <Link to="/contact-me">
           <button className="block w-full text-center px-4 py-3 bg-green-600 text-white rounded font-semibold transition">
